a28eee95c50ddcbea1acd5cf26ad43a037d1631c
Jorgen693 <jorgenhjem@hotmail.com>
2023-05-08 19:34:52 +0200

    Added 3D to 2D visualization

--- src/visualization/3d_gt_plotting.py
+++ src/visualization/3d_gt_plotting.py
@@ -1,44 +1,47 @@
-import os
 import glob
-import matplotlib.pyplot as plt
-import open3d
 from Box3D import *
 
-def render_image_with_boxes(img, objects, calib, frame, time=10):
-    """
-    Show image with 3D boxes
-    """
-    # projection matrix
-    P_rect2cam2 = calib # left rgb camera
-
+def render_image_with_boxes(img, objects, p2Matrix, frame, time=10, results=False):
+    """Function to render the image with the 3D bounding boxes.
+    Args:
+        img (ndarray): the rgb image to render the boxes on
+        objects (list): list of all the objects in the sequence, of type Object3D
+        p2Matrix (ndarray): the projection matrix for the left rgb camera
+        frame (int): the frame number in the sequence 
+        time (int, optional): how long to display. Defaults to 10.
+    """    
     img1 = np.copy(img)
     for obj in objects[frame]:
-        # if obj.type == 'DontCare':
-        #     continue
-        box3d_pixelcoord = map_box_to_image(obj, P_rect2cam2)
-        img1 = draw_projected_box3d(img1, box3d_pixelcoord)
+        box3d_pixelcoord = map_box_to_image(obj, p2Matrix)
+        img1 = draw_projected_box3d(img1, box3d_pixelcoord, obj, frame)
+        img1 = drawXYZlocation(img1, obj, p2Matrix)
 
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-    cv2.imshow("img", img1)
-    cv2.waitKey(time)
-
+    if results:
+        cv2.imshow("results", img1)
+    else:
+        cv2.imshow("ground truth", img1)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
-    ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-    # Load calibration
-    calib = readProjectionMatrices(ROOT_DIR + '/data/video_rect/calib_cam_to_cam.txt')
-    # Load labels
-    labels = load_label(ROOT_DIR + '/data/video_rect/seq_01/labels.txt')
-    labels = load_label(ROOT_DIR + '/results.txt')
-    if(len(labels)==0):
-        print("No labels loaded")
-    labels = sortLabels(labels)
-    # Load images
-    images = glob.glob(ROOT_DIR + '/data/video_rect/seq_01/image_02/data/*.png')
-    images.sort()
-    for i in range(len(labels)):
-        rgb = cv2.cvtColor(cv2.imread(images[i]), 
-                           cv2.COLOR_BGR2RGB)
-        render_image_with_boxes(rgb, labels, calib, i, time=120)
-    cv2.waitKey(0)
+    CAM_TO_CAM = '../data/final_project_2023_rect/calib_cam_to_cam.txt'
+    GROUND_TRUTH = '../data/final_project_2023_rect/seq_01/labels.txt'
+    RESULTS = 'seq_01_results.txt'
+    SEQUENCE = '../data/final_project_2023_rect/seq_01'
+    
+    p2Matrix = readProjectionMatrix(CAM_TO_CAM)         # Load the projection matrix for the left RGB camera
+    gtLabels = load_label(GROUND_TRUTH)                 # Load the labels to use as ground truth
+    resultLabels = load_label(RESULTS, result=True)     # Load the labels of our results
+    gtLabels = sortLabels(gtLabels)                     # Sort the labels by frame number
+    resultLabels = sortLabels(resultLabels)             # Sort the labels by frame number
+    images = glob.glob(SEQUENCE+'/image_02/data/*.png') # Read the images of the sequence
+    images.sort()                                       # Sort the images by frame number
+    # Display the ground truth and the results in two separate windows
+    cv2.imshow("ground truth", cv2.imread(images[0]))
+    cv2.imshow("results", cv2.imread(images[0]))
+    cv2.waitKey(0)                      # Wait for a key press (to be able to start recording video)
+    for i in range(len(resultLabels)):
+        rgb = cv2.cvtColor(cv2.imread(images[i]), cv2.COLOR_BGR2RGB)
+        render_image_with_boxes(rgb, gtLabels, p2Matrix, i, time=80, results=False)
+        render_image_with_boxes(rgb, resultLabels, p2Matrix, i, time=80, results=True)
     cv2.destroyAllWindows()
